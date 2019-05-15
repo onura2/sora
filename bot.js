5b4b3e575ea49df71f265f6b12b19ac92affca13
onura2 <50609671+onura2@users.noreply.github.com>
2019-05-15 16:34:20 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,6 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const ayarlar = require('./ayarlar.json')
 
-
 var prefix = ayarlar.prefix;
 
 client.on('ready', () => {
