cea66d9fbcd901a68cc46d12003a7a079cb9c7a8
onura2 <50609671+onura2@users.noreply.github.com>
2019-05-15 17:08:14 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,31 +10,37 @@ client.on('message', msg => {
     msg.reply('as benı muslumanmı sandın aslında ben bıle bılmıyorum');
   }
 });
+
 client.on('message', msg => {
   if (msg.content === 'shiro') {
     msg.reply('shiro kerdesim olur severim onu :D');
   }
 });
+
 client.on('message', msg => {
   if (msg.content === 'selam') {
     msg.reply('selam bayby ');
   }
 });
+
 client.on('message', msg => {
   if (msg.content === 'kız') {
     msg.reply('gecen bi kız gordum bana sapık dedi .D');
   }
 });
+
 client.on('message', msg => {
   if (msg.content === 'sapık') {
     msg.reply('biri galiba sapık dedi o ben olmayım ?');
   }
 });
+
 client.on('message', msg => {
   if (msg.content === 'neko') {
     msg.reply('i love you neko  :D');
   }
 });
+
 client.on('message', msg => {
   if (msg.content === 'nasılsın') {
     msg.reply('nasılsın sen:D');
